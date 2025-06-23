8c448839e9ae515d69d5eaba89f0f8542cb9bc59
kiVerstehen <marcel.knapp@uni-giessen.de>
2025-06-23 10:42:01 +0200

    added new apps for drawing in plot 006, 013

---
+++ app013/013.py
@@ -0,0 +1,29 @@
+import dash
+from dash import html
+from dash_canvas import DashCanvas
+import base64
+
+# Encode your image file
+def encode_image(image_file):
+    with open(image_file, 'rb') as f:
+        return base64.b64encode(f.read()).decode()
+
+app = dash.Dash(__name__)
+
+image_path = '../Grafiken/NNzumAusfüllen.png'
+image_base64 = encode_image(image_path)
+
+app.layout = html.Div([
+    DashCanvas(
+        id='canvas',
+        width=800,
+        height=600,
+        image_content='data:image/png;base64,{}'.format(image_base64),
+        lineWidth=10,
+        lineColor='blue', 
+        hide_buttons=['zoom', 'pan', 'select', 'rectangle', 'line', 'pencil'],  # Example: hiding some buttons
+    )
+])
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8513)
