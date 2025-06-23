cf52922c7e07c92543ca62c25950e861e1d31446
kiVerstehen <marcel.knapp@uni-giessen.de>
2025-06-23 12:54:19 +0200

    scale adjusted

--- app006/assets/fix_scale.js
+++ app006/assets/fix_scale.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
     const content = document.getElementById('_dash-app-content');
     if (content) {
-        content.style.transform = 'scale(0.8)';
+        content.style.transform = 'scale(0.7)';
         content.style.transformOrigin = '0 0';
         content.style.width = '125%';
         content.style.height = '125%';
