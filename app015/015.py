4db627de1c0356c95f62130ad502db6afbc148d2
kiVerstehen <marcel.knapp@uni-giessen.de>
2025-10-15 14:53:43 +0200

    added relu(2eingänge) app015

---
+++ app015/015.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import plotly.graph_objects as go
+
+import sys
+sys.path.append( '../' )
+from modules.streamlitsliders import SyncedSlider
+
+def plot_with_new_function(w1=0.3, b1=17.0, w2=0.3):
+    #fig, ax = plt.subplots()
+
+    # Neue Funktion definieren
+    def new_function(x,y):
+        z = np.maximum(0, (w1 * x + w2 * y + b1))
+        return z
+    
+    x_vals = np.linspace(0, 3, 100) 
+    y_vals= np.linspace(0, 3, 100)
+    X, Y = np.meshgrid(x_vals, y_vals)
+    Z = new_function(X,Y)
+
+    #x_flat = X.flatten()
+    #y_flat = Y.flatten()
+    #z_flat = Z.flatten()
+    
+    fig = plt.figure()
+    fig = go.Figure(data=[go.Surface(z=Z, x=X, y=Y, colorscale='Viridis')])
+    fig.update_layout(title='Neuron mit zwei Eingängen', scene=dict(
+        xaxis_title='X axis',
+        yaxis_title='Y axis',
+        zaxis_title='Z axis'
+    ))
+
+    st.plotly_chart(fig)
+   
+
+
+# Erstelle zwei Spalten: links für den Slider, rechts für das Diagramm
+col1, col2 = st.columns([1.5, 3])  # Seitenverhältnis: 1 Teil Slider, 3 Teile Plot
+
+with col1:
+    steigung1 = SyncedSlider("Gewicht 1", -3.0, 3.0, 1.0, key_prefix="slider_s1", step=0.1)
+    steigung2 = SyncedSlider("Gewicht 2", -3.0, 3.0, 2.0, key_prefix="slider_s2", step=0.1)
+    bias1 = SyncedSlider('Bias', -5.0, 5.0, -4.0, key_prefix="slider_y1", step=0.1)
+    #st.divider()
+    
+
+with col2:
+    plot_with_new_function(steigung1.value(), bias1.value(), steigung2.value())
